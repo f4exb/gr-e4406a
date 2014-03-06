f6f7e21ff72b303378be0e2b918857e1a1b66356
Edouard Griffiths <f4exb06@gmail.com>
2014-03-06 10:20:10 +0100

    Better QA scenario for double buffered pacer

--- python/qa_dblbuff_pacer_cc.py
+++ python/qa_dblbuff_pacer_cc.py
@@ -32,19 +32,29 @@ class qa_dblbuff_pacer_cc (gr_unittest.TestCase):
         self.tb = None
 
     def test_001_t (self):
-        src = blocks.null_source(gr.sizeof_gr_complex*1)
-        pacer = e4406a.dblbuff_pacer_cc(128, 4)
-        head = blocks.head(gr.sizeof_gr_complex*1, 128*4*2)
+        out_block_size = 8
+        block_factor = 2
+        nb_source_samples = 64
+        src_list = range(nb_source_samples)
+        src_list = map(lambda x: x+0j, src_list)
+        src_data = tuple(src_list)
+        src = blocks.vector_source_c(src_data)
+        pacer = e4406a.dblbuff_pacer_cc(out_block_size, block_factor)
+        head = blocks.head(gr.sizeof_gr_complex*1, nb_source_samples)
         dst = blocks.vector_sink_c()
         # set up fg
         self.tb.connect(src, pacer)
         self.tb.connect(pacer, head)
+        #self.tb.connect(src, head)
         self.tb.connect(head, dst)
         self.tb.run ()
         # check data
         result_data = dst.data()
-        print "# result points:", len(result_data) 
-        self.assertEqual(len(result_data), 128*4*2)
+        print len(result_data), " result points:", result_data
+        for i in range(out_block_size*block_factor):
+            self.assertEqual(result_data[i], 0)
+        for i in range(out_block_size*block_factor,nb_source_samples):
+            self.assertEqual(result_data[i], src_data[i-out_block_size*block_factor])
 
 if __name__ == '__main__':
     gr_unittest.run(qa_dblbuff_pacer_cc, "qa_dblbuff_pacer_cc.xml")
